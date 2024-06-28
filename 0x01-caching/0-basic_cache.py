07da1b8fe40fb2fef87ea86528bb75a51cbff534
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-06-28 10:37:51 +0000

    adding cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,32 +1,20 @@
-#!/usr/bin/env python3
-""" BaseCaching module
+#!/usr/bin/python3
+""" BasicCache module
 """
 
-
 from base_caching import BaseCaching
+
+
 class BasicCache(BaseCaching):
-    """BaseCaching defines:
-      - a dictionary `cache_data`
-    """
-
-    def __init__(self):
-        """Init the class
-        """
-        self.cache_data = {}
-
-    def print_cache(self):
-        """Print the cache
-        """
-        print("Current cache:")
-        for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key, self.cache_data[key]))
+    """ BasicCache defines a caching system with no limit """
 
     def put(self, key, item):
-        """ Add an item in the cache
-        """
-        raise NotImplementedError("put must be implemented in your cache class")
+        """ Add an item in the cache """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
 
     def get(self, key):
-        """ Get an item by key
-        """
-        raise NotImplementedError("get must be implemented in your cache class")
+        """ Get an item by key """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
