6ef98576d1a172ca70264955354ffb5a15e033dc
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-06-28 12:49:47 +0000

    lifo

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" LIFOCache module """
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ LIFOCache defines a caching system with LIFO eviction policy """
+
+    def __init__(self):
+        """ Initialize the class """
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.order.remove(key)
+
+        self.cache_data[key] = item
+        self.order.append(key)
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            last_key = self.order.pop()
+            del self.cache_data[last_key]
+            print("DISCARD:", last_key)
+
+    def get(self, key):
+        """ Get an item by key """
+        return self.cache_data.get(key, None)
