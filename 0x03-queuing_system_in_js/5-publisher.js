ae49c1c4cff5650e6178fe447ccaed61f53b39df
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-07-30 21:32:21 -0700

    Node Redis client publisher and subscriber

---
+++ 0x03-queuing_system_in_js/5-publisher.js
@@ -0,0 +1,27 @@
+import redis from 'redis';
+
+// Create Redis client
+const publisher = redis.createClient();
+
+// Log connection and error events
+publisher.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+publisher.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+// Function to publish a message after a delay
+function publishMessage(message, time) {
+  setTimeout(() => {
+    console.log(`About to send ${message}`);
+    publisher.publish('holberton school channel', message);
+  }, time);
+}
+
+// Publish messages with delays
+publishMessage('Holberton Student #1 starts course', 100);
+publishMessage('Holberton Student #2 starts course', 200);
+publishMessage('KILL_SERVER', 300);
+publishMessage('Holberton Student #3 starts course', 400);
