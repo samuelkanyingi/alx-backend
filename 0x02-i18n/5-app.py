f400cd58789be4666cb377a0dfd54a6f9bd854dd
root <samuelkanyingi2016@gmail.com>
2024-08-01 01:51:29 +0000

    Mock logging in

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+"""
+Flask app with Babel integration for i18n and translations.
+"""
+
+from flask import Flask, render_template, request, g
+from flask_babel import Babel, _
+from typing import Optional, Dict
+
+class Config:
+    """
+    Configuration class for Flask app.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user() -> Optional[Dict]:
+    """
+    Get user based on login_as parameter.
+    """
+    user_id = request.args.get('login_as')
+    if user_id:
+        return users.get(int(user_id))
+    return None
+
+
+@app.before_request
+def before_request():
+    """
+    Function to run before each request.
+    """
+    g.user = get_user()
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Determine the best match for supported languages based on the request.
+    If a locale parameter is present in the request and its value is supported
+    locale, use it; otherwise, use the default behavior.
+
+    Returns:
+        str: The best match language.
+    """
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    if g.user and g.user.get('locale') in app.config['LANGUAGES']:
+        return g.user['locale']
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> str:
+    """
+    Render the index.html template.
+
+    Returns:
+        str: Rendered HTML content of the index template.
+    """
+    return render_template('5-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
